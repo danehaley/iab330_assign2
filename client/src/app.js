1e780e63d28dbc9d922586b594c9d4a90270f5a6
Dane Dobra <danehaley22@gmail.com>
2022-10-13 18:53:26 +1000

    init

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+// Components
+import Header from "../src/components/header.js";
+
+function App() {
+  return (
+    <div className="app">
+      {Header()}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+      <p>寿司</p>
+    </header>
+  );
+}
