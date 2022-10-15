59371eb675a0c1efa4e49b0e52c229bca71346aa
Dane Haley <danehaley22@gmail.com>
2022-10-15 10:05:11 +1000

    Additional maintenance card styling

--- client/src/routes/maintenance.jsx
+++ client/src/routes/maintenance.jsx
@@ -33,23 +33,40 @@ export default class Maintenance extends React.Component {
 
 function CreateCard(props) {
   return (
-    <Col xs={12} md={6} lg={4} className="p-1">
-      <Card className="w-100">
-        <Card.Body>
-          <Card.Title>{props.name}</Card.Title>
-          <Card.Text>{props.availability}</Card.Text>
-          <Card.Text>{props.currentPop} Currently</Card.Text>
-          <Card.Text>{props.sinceCleanPop} Since Clean</Card.Text>
-          <Card.Text>Last Cleaned: {props.lastCleanDate}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+    <>
+      <Col xs={12} md={6} lg={4} className="p-2">
+        <Card className="w-100 rounded-4">
+          <Card.Body>
+            <Card.Title>{props.name}</Card.Title>
+            <Card.Text>{props.availability}</Card.Text>
+            <Card.Text>{props.currentPop} Currently</Card.Text>
+            <Card.Text>{props.sinceCleanPop} Since Clean</Card.Text>
+            <Card.Text>Last Cleaned: {props.lastCleanDate}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      {/* Custom Bootstrap styles */}
+      <style type="text/css">
+        {`
+        .card{
+          transition: all .3s cubic-bezier(0,0,.5,1);
+          cursor: pointer;
+          box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
+        }
+        .card:hover {
+          box-shadow: 0 .25rem .5rem rgba(0,0,0,.16);
+          transform: scale3d(1.01,1.01,1.01);
+        }
+      `}
+      </style>
+    </>
   );
 }
 
 function CreateCardList() {
   return (
-    <Container>
+    <Container className="px-4" style={{ maxWidth: "1320px" }}>
       <Row>
         <CreateCard
           name={"ROOM 1X"}
