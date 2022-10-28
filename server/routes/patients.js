d4543083e4dd6121600b8232c2dfd20d695c4530
Dane Haley <danehaley22@gmail.com>
2022-10-28 12:55:08 +1000

    patients fix

--- server/routes/patients.js
+++ server/routes/patients.js
@@ -6,16 +6,10 @@ const router = express.Router();
 router.get("/patients", (req, res) => {
   const query = `
     SELECT *
-    FROM patientinfo;
+    FROM patientinfo2;
     `;
-  client.query(query, (err, res) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    for (let row of res.rows) {
-      console.log(row);
-    }
+  client.query(query).then((results) => {
+    res.status(200).json(results.rows);
   });
 });
 
